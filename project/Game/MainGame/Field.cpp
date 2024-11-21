47abd70f9cd922a5616e56b98283e98cd16e330b
sxavenger <sxavenger.x3@gmail.com>
2024-11-21 14:56:41 +0900

    fieldの追加, test groundの削除

--- project/Game/MainGame/Field.cpp
+++ project/Game/MainGame/Field.cpp
@@ -1,4 +1,5 @@
 #include "Field.h"
+_DXOBJECT_USING
 
 //-----------------------------------------------------------------------------------------
 // include
@@ -21,6 +22,19 @@ void FieldTree::Init(uint32_t id, DeltaTimePoint startTime) {
 	AnimationBehavior::TryLoadJson();
 
 	animationTimer_ = startTime;
+
+	exporter_.TryLoadFromJson();
+
+	matrix_ = std::make_unique<BufferResource<TransformationMatrix>>(Sxavenger::GetDevicesObj(), kInstanceCount_);
+
+	for (uint32_t i = 0; i < kInstanceCount_; ++i) {
+
+		// stashからの取り出し
+		exporter_.GetFromStash(std::string("position" + std::to_string(i)).c_str(), &positions_[i].x, 3);
+
+		// matrixに設定
+		(*matrix_)[i].Transfer(Matrix::MakeTranslate(positions_[i]));
+	}
 }
 
 void FieldTree::Term() {
@@ -33,6 +47,45 @@ void FieldTree::Update() {
 	animator_->Update(animationTimer_, 0, true);
 }
 
+void FieldTree::DrawSystematic(_MAYBE_UNUSED const Camera3D* camera) {
+	if (model_ == nullptr) {
+		return; //!< modelが設定されていない
+	}
+
+	//!< animatorの描画
+
+	auto commandList = Sxavenger::GetCommandList();
+
+	// TODO: mesh shaderへの対応
+	sSystemConsole->SetRenderingPipeline(kDefaultVS_AlbedoPS_Deferred);
+
+	for (uint32_t i = 0; i < model_->GetMeshSize(); ++i) {
+		// animator適用のIAを設定
+		animator_->BindIABuffer(i);
+
+		commandList->SetGraphicsRootConstantBufferView(0, camera->GetGPUVirtualAddress());
+		commandList->SetGraphicsRootShaderResourceView(1, matrix_->GetGPUVirtualAddress());
+		commandList->SetGraphicsRootConstantBufferView(2, uvTransform_.GetVirtualAddress());
+		commandList->SetGraphicsRootDescriptorTable(3, model_->GetTextureHandle(i));
+		commandList->SetGraphicsRootConstantBufferView(4, material_.GetGPUVirtualAddress());
+		commandList->SetGraphicsRootConstantBufferView(5, color_.GetGPUVirtualAddress());
+
+		model_->GetMesh(i).DrawCall(kInstanceCount_);
+	}
+}
+
+void FieldTree::SetAttributeImGui() {
+	for (uint32_t i = 0; i < kInstanceCount_; ++i) {
+		if (exporter_.DragFloat3(std::string("position" + std::to_string(i)).c_str(), &positions_[i].x, 0.02f)) {
+			(*matrix_)[i].Transfer(Matrix::MakeTranslate(positions_[i]));
+		}
+	}
+
+	if (ImGui::Button("parameter export")) {
+		exporter_.OutputToJson();
+	}
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////
 // Field class methods
 ////////////////////////////////////////////////////////////////////////////////////////////
@@ -43,6 +96,11 @@ void Field::Init() {
 	}
 
 	SetName("field");
+
+	ModelBehavior::model_ = SxavengerGame::LoadModel("resourcesData/GameScene/Model", "field.obj");
+	ModelBehavior::model_->ApplyMeshShader();
+	ModelBehavior::renderingFlag_ = kBehaviorRender_Systematic;
+	ModelBehavior::TryLoadJson();
 }
 
 void Field::Term() {
