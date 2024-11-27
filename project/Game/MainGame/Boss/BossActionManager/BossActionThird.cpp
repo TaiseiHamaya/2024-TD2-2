1961d7853fd13a536df549b52c4e87ccdfb178bc
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2024-11-27 15:41:19 +0900

    敵アクションに関するImGuiを作成しました。

--- project/Game/MainGame/Boss/BossActionManager/BossActionThird.cpp
+++ project/Game/MainGame/Boss/BossActionManager/BossActionThird.cpp
@@ -23,22 +23,35 @@ BossActionThird::BossActionThird() {
 		"TongueStart", "TonguePlay", "TongueEnd" };
 	newFlow.coolTime = 5.0f;
 	maxHitpoint = 10;
+
+	SetName("BossActionThird");
+	exporter_.TryLoadFromJson();
+
+	exporter_.GetFromStash("StayTime", &StayTime, 1);
+	exporter_.GetFromStash("LookAtStartTime", &LookAtStartTime, 1);
+	exporter_.GetFromStash("RushStartTime", &RushStartTime, 1);
+	exporter_.GetFromStash("RushSpeed", &RushSpeed, 1);
+	exporter_.GetFromStash("RushLength", &RushLength, 1);
+	exporter_.GetFromStash("ColliderLength", &ColliderLength, 1);
+	exporter_.GetFromStash("TurnTime", &TurnTime, 1);
+
+	SetToConsole();
 }
 
 std::unique_ptr<BaseBossBehavior> BossActionThird::create(const std::string& behaviorName) {
 	if (behaviorName == "Stay") {
-		return std::make_unique<BossBehaviorStay>(3.0f, playerManager); // 待機時間, 2つ目は必要なのでこれ
+		return std::make_unique<BossBehaviorStay>(StayTime, playerManager); // 待機時間, 2つ目は必要なのでこれ
 	}
 	else if (behaviorName == "RushStartR") {
 		return std::make_unique<BossBehaviorRushStart>(
-			MakeAxisAngle({ 0,1,0 }, pi_v / 2), 1.0f, 2.0f);
+			MakeAxisAngle({ 0,1,0 }, pi_v / 2), LookAtStartTime, RushStartTime);
 	}
 	else if (behaviorName == "RushStartL") {
 		return std::make_unique<BossBehaviorRushStart>(
-			MakeAxisAngle({ 0,1,0 }, -pi_v / 2), 1.0f, 2.0f); // 向き, 回転開始までの時間, 全体の時間
+			MakeAxisAngle({ 0,1,0 }, -pi_v / 2), LookAtStartTime, RushStartTime); // 向き, 回転開始までの時間, 全体の時間
 	}
 	else if (behaviorName == "RushPlay") {
-		return std::make_unique<BossBehaviorRushPlay>(3.0f, 5.0f); // 突進長, 速度
+		return std::make_unique<BossBehaviorRushPlay>(RushSpeed, RushLength); // 突進長, 速度
 	}
 	else if (behaviorName == "RushEnd") {
 		return std::make_unique<BossBehaviorRushEnd>();
@@ -47,11 +60,27 @@ std::unique_ptr<BaseBossBehavior> BossActionThird::create(const std::string& beh
 		return std::make_unique<BossBehaviorTongueStart>(); // 
 	}
 	else if (behaviorName == "TonguePlay") {
-		return std::make_unique<BossBehaviorTonguePlay>(20.0f, 1.0f); // コリジョンの長さ, 回転にかかる時間
+		return std::make_unique<BossBehaviorTonguePlay>(ColliderLength, TurnTime); // コリジョンの長さ, 回転にかかる時間
 	}
 	else if (behaviorName == "TongueEnd") {
 		return std::make_unique<BossBehaviorTongueEnd>(); // 
 	}
 
 	return nullptr;
+}
+
+void BossActionThird::SetAttributeImGui() {
+	exporter_.DragFloat("StayTime", &StayTime, 0.1f);
+	exporter_.DragFloat("LookAtStartTime", &LookAtStartTime, 0.1f);
+	exporter_.DragFloat("RushStartTime", &RushStartTime, 0.1f);
+	exporter_.DragFloat("RushSpeed", &RushSpeed, 0.1f);
+	exporter_.DragFloat("RushLength", &RushLength, 0.1f);
+	exporter_.DragFloat("ColliderLength", &ColliderLength, 0.1f);
+	exporter_.DragFloat("TurnTime", &TurnTime, 0.1f);
+
+	//exporter_.("RushLength", &RushLength, 0.1f);
+
+	if (ImGui::Button("Export")) {
+		exporter_.OutputToJson();
+	}
 }
